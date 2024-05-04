8ae6e94c95153cf5b53250d52cc60afd0ecfb287
Blealtan Cao <blealtan@outlook.com>
2024-05-04 14:59:47 +0800

    Add mnist example.

---
+++ examples/mnist.py
@@ -0,0 +1,68 @@
+from efficient_kan import KAN
+
+# Train on MNIST
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+# Load MNIST
+transform = transforms.Compose(
+    [transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))]
+)
+trainset = torchvision.datasets.MNIST(
+    root="./data", train=True, download=True, transform=transform
+)
+valset = torchvision.datasets.MNIST(
+    root="./data", train=False, download=True, transform=transform
+)
+trainloader = DataLoader(trainset, batch_size=64, shuffle=True)
+valloader = DataLoader(valset, batch_size=64, shuffle=False)
+
+# Define model
+model = KAN([28 * 28, 10])
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+
+# Define optimizer
+optimizer = optim.Adam(model.parameters(), lr=1e-3)
+
+# Define loss
+criterion = nn.CrossEntropyLoss()
+
+for epoch in range(10):
+    # Train
+    model.train()
+    with tqdm(trainloader) as pbar:
+        for i, (images, labels) in enumerate(pbar):
+            images = images.view(-1, 28 * 28).to(device)
+            optimizer.zero_grad()
+            output = model(images)
+            loss = criterion(output, labels.to(device))
+            loss.backward()
+            optimizer.step()
+            accuracy = (output.argmax(dim=1) == labels.to(device)).float().mean()
+            pbar.set_postfix(loss=loss.item(), accuracy=accuracy.item())
+
+    # Validation
+    model.eval()
+    val_loss = 0
+    val_accuracy = 0
+    with torch.no_grad():
+        for images, labels in valloader:
+            images = images.view(-1, 28 * 28).to(device)
+            output = model(images)
+            val_loss += criterion(output, labels.to(device)).item()
+            val_accuracy += (
+                (output.argmax(dim=1) == labels.to(device)).float().mean().item()
+            )
+
+    val_loss /= len(valloader)
+    val_accuracy /= len(valloader)
+
+    print(
+        f"Epoch {epoch + 1}, Val Loss: {val_loss}, Val Accuracy: {val_accuracy}"
+    )
